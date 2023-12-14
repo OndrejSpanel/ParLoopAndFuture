408ed8981859e4f23b059bbcbc810b8af4988346
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2023-12-14 17:10:42 +0100

    Use thread pool for custom EC

--- src/main/scala/com/github/ondrejspanel/parFuture/CustomEC.scala
+++ src/main/scala/com/github/ondrejspanel/parFuture/CustomEC.scala
@@ -1,8 +1,23 @@
 package com.github.ondrejspanel.parFuture
 
+import java.util.concurrent.{Executors, ThreadFactory}
 import scala.concurrent._
 
 object CustomEC {
-  final lazy implicit val custom: ExecutionContextExecutor = ExecutionContext.fromExecutor(null)
+  // many new Intel CPUs report plenty of cores, as they include "economical" cores as well
+  // for our purposes, we do not want to run on them. Extra high concurrency levels make no sense anyway
+  val maxCPUs = 8
+
+  val daemonThreadFactory: ThreadFactory = new ThreadFactory {
+    val defaultFactory = Executors.defaultThreadFactory()
+
+    def newThread(r: Runnable): Thread = {
+      val thread = defaultFactory.newThread(r)
+      thread.setDaemon(true)
+      thread
+    }
+  }
+  val threadPool = Executors.newFixedThreadPool(scala.collection.parallel.availableProcessors min maxCPUs, daemonThreadFactory)
+  final lazy implicit val custom: ExecutionContextExecutor = ExecutionContext.fromExecutor(threadPool)
 
 }
